0e53f49d57a0ecc3dea80ef0dce2e2df4c104ba1
Guilherme <gui-rock02@Hotmail.com>
2023-04-30 23:53:58 -0300

    added readme

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,13 +4,6 @@ const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
-
-
-
-
-
-
-
 const usersController = new UsersController
 
 usersRoutes.post("/",usersController.create);
